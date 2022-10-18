26c6e6068ffc8f24053348d789e667be566fd136
mmiesch <mark.s.miesch@gmail.com>
2022-10-18 13:08:07 -0600

    viz bins

--- specfig.py
+++ specfig.py
@@ -13,47 +13,69 @@ x1 = np.array([20.4, 22.0, 24.4, 27.6, 30.8, 34.0, 38.8, 45.2, 51.6, 58.0, \
 #------------------------------------------------------------------------------
 # spectrum
 
-N1 = len(x1)
+Ni = len(x1)
 
-ee = - 2.0
+ee = - 1.5
 
-y1 = np.power(x1,ee)
+y1 = 1.e6 * np.power(x1,ee)
 
 #------------------------------------------------------------------------------
 # Rebin ions
 # assume the L2 ion bin number is odd, and keep the last bin intact
 
-Mi = int((N1-1)/2) + 1
+Mi = int((Ni-1)/2) + 1
 print(f"Mi = {Mi}")
 
 x2 = np.zeros(Mi)
 y2 = np.zeros(Mi)
-e1 = np.sqrt(x1[0]**3/x2[1])
-e2 = np.sqrt(x1[1]*x2[2])
+e1 = np.sqrt(x1[0]**3/x1[1])
+e2 = np.sqrt(x1[1]*x1[2])
 x2[0] = np.sqrt(e1*e2)
 y2[0] = y1[0]+y1[1]
 
 idx = 1
-for i in np.arange(2,N1-2,2):
+for i in np.arange(2,Ni-2,2):
         mm = x1[i-1]*x1[i]*x1[i+1]*x1[i+2]
         x2[idx] = np.power(mm, 0.25)
         y2[idx] = y1[i] + y1[i+1]
         idx += 1
 
+x2[-1] = x1[-1]
 y2[-1] = y1[-1]
+
+for i in np.arange(Mi):
+    print(f"{x2[i]} {y2[i]}")
 
 #------------------------------------------------------------------------------
 
+y1min = y1 - np.sqrt(y1)
+y1max = y1 + np.sqrt(y1)
+
+y2min = y2 - np.sqrt(y2)
+y2max = y2 + np.sqrt(y2)
+
+#------------------------------------------------------------------------------
+# scale factor
+fac = 0.5
+
 fig = plt.figure(figsize=(16,8))
 
-ax1 = fig.add_subplot(2, 1, 1)
+ax1 = fig.add_subplot(1, 1, 1)
 ax1.set_xscale('log')
 ax1.set_yscale('log')
 ax1.plot(x1,y1)
-ax1.plot(x2, y2, 'o')
+ax1.plot(x1,y1,'o')
+#ax1.plot(x2, fac*y2, 'o')
+ax1.plot(x2, y2)
+ax1.plot(x2, y2,'o')
 
-#ax2 = fig.add_subplot(2, 2, 2)
-#ax2.plot(di)
-#ax2.plot(di, 'o')
+plt.fill_between(x1,y1min,y1max,color='lightsteelblue')
+plt.fill_between(x2,y2min,y2max,color='bisque')
+
+yy = [1.0,1.e5]
+
+for i in np.arange(1,Ni-1,2):
+        ed = np.sqrt(x1[i]*x1[i+1]) * np.array((1.0,1.0))
+        ax1.plot(ed,yy,color='b')
 
 plt.show()
